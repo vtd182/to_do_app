b1d9386cebbf46a3eca59d34a3af95d0b0735444
vtd182 <92201436+vtd182@users.noreply.github.com>
2024-10-28 09:43:11 +0700

    request permission

--- lib/domain/data_source/notification_service.dart
+++ lib/domain/data_source/notification_service.dart
@@ -6,10 +6,13 @@ class NotificationService {
   final FlutterLocalNotificationsPlugin _flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
 
   Future<void> init() async {
+    // Khởi tạo múi giờ
     tz.initializeTimeZones();
 
+    // Thiết lập cho Android
     const AndroidInitializationSettings initializationSettingsAndroid = AndroidInitializationSettings('@mipmap/ic_launcher');
 
+    // Thiết lập cho iOS với yêu cầu quyền thông báo
     const DarwinInitializationSettings initializationSettingsIOS =
         DarwinInitializationSettings(requestAlertPermission: true, requestBadgePermission: true, requestSoundPermission: true);
 
@@ -18,7 +21,25 @@ class NotificationService {
       iOS: initializationSettingsIOS,
     );
 
+    // Khởi tạo Flutter Local Notifications Plugin
     await _flutterLocalNotificationsPlugin.initialize(initializationSettings);
+
+    // Yêu cầu quyền thông báo cho iOS và Android 13 trở lên
+    await _requestPermissions();
+  }
+
+  Future<void> _requestPermissions() async {
+    // Yêu cầu quyền cho iOS
+    await _flutterLocalNotificationsPlugin.resolvePlatformSpecificImplementation<IOSFlutterLocalNotificationsPlugin>()?.requestPermissions(
+          alert: true,
+          badge: true,
+          sound: true,
+        );
+
+    // Yêu cầu quyền cho Android 13 trở lên
+    await _flutterLocalNotificationsPlugin
+        .resolvePlatformSpecificImplementation<AndroidFlutterLocalNotificationsPlugin>()
+        ?.requestNotificationsPermission();
   }
 
   Future<void> scheduleNotification(int id, String title, String body, DateTime scheduledDate) async {
